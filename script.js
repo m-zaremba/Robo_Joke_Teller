79608291717ae28bf40d60e69b33e0c85b8777b6
m-zaremba <zaremba_michal@wp.pl>
2021-11-12 17:09:15 +0100

    fetch a joke and text to speech implementation

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello");
+  const button = document.getElementById("button");
+
+  const formatJoke = (data) => {
+    let joke = "";
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    return joke;
+  };
+
+  const getJokes = async () => {
+    const apiUrl = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getJokes().then((data) => formatJoke(data));
+
 });
